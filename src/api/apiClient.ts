b73241145298cf5651f3fcd74474aa446bcaffcc
Akshay Sapatee <akshay.sapate@haptiq.com>
2026-05-19 17:12:33 +0530

    refactor: using ENV variable for baseURL

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,14 +1,11 @@
-
-
 import { request, APIRequestContext } from '@playwright/test';
-import { ENV } from '../../config/env'; // Import your environment config
+import { ENV } from '../../config/env'; 
 
 export async function createAPIContext(): Promise<APIRequestContext> {
     return await request.newContext({
-        baseURL: ENV.baseURL, // Use the variable instead of hardcoding
+        baseURL: ENV.baseURL, // <--- Use this instead of the hardcoded string
         extraHTTPHeaders: {
             Accept: 'application/json',
         },
     });
 }
-
