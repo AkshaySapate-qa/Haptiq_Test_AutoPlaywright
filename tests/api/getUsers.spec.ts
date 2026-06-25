a969100dd3cdd1f978d7c499634e14cbc26ce401
Akshay Sapatee <akshay.sapate@haptiq.com>
2026-06-25 16:13:13 +0530

    Added a new comment to the getUsers test

--- tests/api/getUsers.spec.ts
+++ tests/api/getUsers.spec.ts
@@ -12,7 +12,7 @@ test.beforeAll(async () => {
 
 test.afterAll(async () => {
   await apiContext.dispose();
-});
+}); // test result git status
 
 test.describe('GET Users API Tests', () => {
   test('Verify GET /users returns list of users successfully', async () => {
